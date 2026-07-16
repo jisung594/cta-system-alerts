dabf2eff14f49870b0bd431a1f8c6a3f1bd3e52a
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-07-16 15:41:54 -0500

    chore: add placeholder text

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
-    <>
-    
-      <>HELLOWORLD</>
-    </>
+    <main className="">
+      <div className="bg-slate-900 text-slate-100 flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold">Realtime CTA System Alerts</h1>
+        <h1 className="text-3xl font-bold">dashboard + data viz</h1>
+      </div>
+    </main>
   )
 }
 
