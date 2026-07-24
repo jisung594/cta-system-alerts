2213b790ce4014c60a77635f86438a6dce625f54
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-07-24 12:07:33 -0500

    feat: integrate CTAAlertsDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import './App.css'
+import CTAAlertsDashboard from './components/CTAAlertsDashboard'
 
 function App() {
-  return (
-    <main className="">
-      <div className="bg-slate-900 text-slate-100 flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">Realtime CTA System Alerts</h1>
-        <h1 className="text-3xl font-bold">dashboard + data viz</h1>
-      </div>
-    </main>
-  )
+  return <CTAAlertsDashboard />
 }
 
 export default App
